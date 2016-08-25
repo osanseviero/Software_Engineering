a69ed0b4be57024135efeb58ecb6a9aa3bda0eee
Omar S <osanseviero@gmail.com>
2016-08-25 09:17:52 -0500

    Fixed cloned computer

--- Tarea3/cloneComputer.h
+++ Tarea3/cloneComputer.h
@@ -2,6 +2,6 @@ template <class Base, class SubClass>
 class CloneComputer : public Computer {
 public:
 	virtual Base* clone() {
-		return new SubClass(dynamic_cast<SubClass&>(*this));
+		return new SubClass(static_cast<SubClass&>(*this));
 	}
 };
