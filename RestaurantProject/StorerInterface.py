cb4b58ffac4473e827316222679129a659785f74
Omar S <osanseviero@gmail.com>
2016-11-22 02:47:05 -0600

    feat: Permit the warehouser to complete kitchen requests

--- RestaurantProject/StorerInterface.py
+++ RestaurantProject/StorerInterface.py
@@ -30,13 +30,24 @@ class Storer():
 		ingredient = helper.findStoredIngredientById(request[0])
 		if ingredient == None:
 			print "This ingredient is not in the warehouse, please request more."
-		print request[1]
-
-		# If there is more of this instance of the ingredient at the warehouse than the requested
-		if ingredient['quantity'] > request[1]:
-			ingredient['quantity'] -= request[1]
-			helper.updateStoredIngredient(ingredient['_id'], ingredient['quantity'])
-
+		else:
+			keepTrying = True
+			while request[1] > 0 and keepTrying:
+				if int(ingredient['quantity']) >= int(request[1]):
+					ingredient['quantity'] -= request[1]
+					request[1] = 0
+					print ingredient['quantity'], " and quantity: ", request[1]
+					helper.deleteIngredient(request[0])
+					helper.updateStoredIngredient(ingredient['_id'], ingredient['quantity'])
+					keepTrying = False
+				else:
+					request[1] -= ingredient['quantity']
+					helper.updateStoredIngredient(ingredient['_id'], 0)
+					ingredient = helper.findStoredIngredientById(request[0])
+					if ingredient == None:
+						print "There was not enough of the ingredient in the warehouse to fulfill your request, sir."
+						keepTrying = False
+						helper.updateRequestIngredient(request[0], request[1])
 
 	def interface(self):
 		helper.clearWindow()
